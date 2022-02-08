464d23b383091d4eb64094e5bfcfd9ea1a598cd4
KDW <42798237+dawan0111@users.noreply.github.com>
2022-02-09 01:45:20 +0900

    Update arduino.ino
    cmd_vel topic 구독 및 Twist3D 값에 따라 모터 목표 속도 변경

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,9 +1,6 @@
 #include <TimerFive.h>
 #include <ros.h>
-#include <std_msgs/Int8.h>
-#include <std_msgs/Float32.h>
-#include <std_msgs/Float32MultiArray.h>
-
+#include <geometry_msgs/Twist.h>
 #include "MotorPID.h"
 #define MOTOR1_EN_A 18
 #define MOTOR1_EN_B 19
@@ -14,6 +11,8 @@
 #define MOTOR2_EN_B 2
 #define MOTOR2_PWM 6
 #define MOTOR2_DIR 7
+
+#define PI 3.1415926535897932384626433832795
 
 MotorPID motor1PID(0.1, 0.1, 2, 13, 100, 50);
 MotorPID motor2PID(0.1, 0.1, 2, 13, 100, 50);
@@ -36,14 +35,21 @@ void motor2EnBISR(){
 
 ros::NodeHandle nh;
 
-void messageCb( const std_msgs::Int8& toggle_msg){
-  motor2PID.setGoalSpeed(toggle_msg.data);
+void messageCb( const geometry_msgs::Twist& goalTwist){
+  float linearSpeed = goalTwist.linear.x;
+  float angularSpeed = goalTwist.angular.z;
+
+  float motor1Speed = (float) linearSpeed + (float) (angularSpeed * 0.1075);
+  float motor2Speed = (float) linearSpeed + (float) (angularSpeed * -0.1075);
+
+  float motor1RPM = (float) motor1Speed / 0.066 / PI * 60.0;
+  float motor2RPM = (float) motor2Speed / 0.066 / PI * 60.0;
+
+  motor1PID.setGoalSpeed(motor1RPM);
+  motor2PID.setGoalSpeed(motor2RPM);
 }
 
-
-std_msgs::Float32MultiArray pubMotorSpeed;
-ros::Subscriber<std_msgs::Int8> sub("toggle_led", &messageCb );
-ros::Publisher odometryPub("odometry", &pubMotorSpeed);
+ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", &messageCb );
 
 void setup() {
   Timer5.initialize(50000);
@@ -65,29 +71,30 @@ void setup() {
   pinMode(MOTOR2_DIR, OUTPUT);
   pinMode(MOTOR2_PWM, OUTPUT);
 
-  Serial.begin(57600);
+  Serial.begin(115200);
 
-  // motor2PID.setGoalSpeed(15);
+  motor1PID.setGoalSpeed(0);
+  motor2PID.setGoalSpeed(0);
+
   nh.initNode();
   nh.subscribe(sub);
-  nh.advertise(odometryPub);
-
-  pubMotorSpeed.data = (float*)malloc(sizeof(float) * 2);
-  pubMotorSpeed.data_length = 2;
 }
 
-void loop() {;
-   nh.spinOnce();
+void loop() {
+  Serial.print(motor1PID.getCurrentSpeed(), 10);
+  Serial.println(motor2PID.getCurrentSpeed(), 10);
+  nh.spinOnce();
 }
 
 void timer5_ISR(){
+  int motor1Value[2] = {0, 0};
+  motor1PID.control(motor1Value);
+  
   int motor2Value[2] = {0, 0};
   motor2PID.control(motor2Value);
-
-  pubMotorSpeed.data[0] = motor2PID.getCurrentSpeed();
-  pubMotorSpeed.data[1] = motor2PID.getCurrentSpeed();
-  odometryPub.publish(&pubMotorSpeed);
   
+  digitalWrite(MOTOR1_DIR, motor1Value[0] == 1 ? HIGH : LOW);
+  analogWrite(MOTOR1_PWM, motor1Value[1]);
   digitalWrite(MOTOR2_DIR, motor2Value[0] == 1 ? HIGH : LOW);
-  analogWrite(MOTOR2_PWM, motor2Value[1]); 
+  analogWrite(MOTOR2_PWM, motor2Value[1]);
 }
